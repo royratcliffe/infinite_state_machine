004297e93e904ec1e62e5c665ed0c21b3d39c07e
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-29 20:09:44 +0100

    Implement state machine test in abc.cpp with state transitions and assertions

--- test/abc.cpp
+++ test/abc.cpp
@@ -1,1 +1,30 @@
-extern "C" int test_abc() { return 0; }
+#include "infinite_state_machine.hpp"
+
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+struct my_state : infinite::state<my_state> {
+  const char *name;
+};
+
+static my_state a = {nullptr, "a"};
+static my_state b = {&a, "b"};
+static my_state c = {&b, "c"};
+
+static infinite::state_machine<my_state> ism;
+
+extern "C" int test_abc() {
+  auto done = ism.go(&c);
+  for (const auto &state : done.exits) {
+    cout << "Exited state: " << state->self()->name << endl;
+  }
+  for (const auto &state : done.entries) {
+    cout << "Entered state: " << state->self()->name << endl;
+  }
+  assert(ism.at() == &c);
+  assert(ism.in(&a));
+  assert(ism.in(&b));
+  return 0;
+}

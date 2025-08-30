fce3461f918ef551254fc856edf8ab24f2825577
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-30 14:54:25 +0100

    Refactor output formatting in test_abc to improve clarity and consistency

--- test/abc.cpp
+++ test/abc.cpp
@@ -12,6 +12,7 @@ struct my_state : infinite::state<my_state> {
 static my_state a = {nullptr, "a"};
 static my_state b = {&a, "b"};
 static my_state c = {&b, "c"};
+static my_state d = {&a, "d"};
 
 static infinite::state_machine<my_state> ism;
 
@@ -26,30 +27,34 @@ ostream &operator<<(ostream &os, const my_state *s) {
 ostream &
 operator<<(ostream &os,
            const infinite::state_machine<my_state>::transition &transition) {
-  os << "Exited states:";
+  os << "exited (";
   for (const auto &state : transition.exits) {
     os << " " << state->self()->name;
   }
-  os << endl;
-  os << "Entered states:";
+  os << " ) entered (";
   for (const auto &state : transition.enters) {
     os << " " << state->self()->name;
   }
-  os << endl;
+  os << " )";
   return os;
 }
 
 extern "C" int test_abc() {
-  cout << ism.go(&c);
+  cout << ism.go(&c) << endl;
   assert(ism.at() == &c);
   assert(ism.in(&a));
   assert(ism.in(&b));
   auto transition = ism.go(&a);
-  cout << transition;
+  cout << transition << endl;
   assert(transition.enters.empty());
   assert(transition.exits.size() == 2);
   assert(transition.exits[0] == &c);
   assert(transition.exits[1] == &b);
   assert(ism.at() == &a);
+  assert(ism.in(&a));
+  assert(!ism.in(&b));
+  assert(!ism.in(&c));
+  cout << "transition to c: " << ism.go(&c) << endl;
+  cout << "transition to d: " << ism.go(&d) << endl;
   return 0;
 }

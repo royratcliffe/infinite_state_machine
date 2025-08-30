7384f0afe0d6ebd80c0109a3e1bb0a0cc53ac135
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-30 14:28:54 +0100

    Refactor infinite_state structure documentation for clarity and completeness

--- inc/infinite_state.h
+++ inc/infinite_state.h
@@ -6,10 +6,41 @@
  */
 struct infinite_state_machine;
 
+/*!
+ * \brief The state structure for the infinite state.
+ * This structure represents a state in the infinite state machine. It contains
+ * information about the state's parent, composite enter action, and composite
+ * exit action.
+ */
 struct infinite_state
 {
+    /*!
+     * \brief The parent state of this state.
+     */
     struct infinite_state *super;
+
+    /*!
+     * \brief The enter action for this state.
+     * \note This function is called when the state is entered in response to a
+     * transition. The state is already pushed onto the machine stack when this
+     * function is called. The function may be \c NULL, in which case no action
+     * is taken.
+     *
+     * \param state The incoming sub-state.
+     * \param machine The infinite state machine.
+     */
     void (*enter)(struct infinite_state *state, struct infinite_state_machine *machine);
+
+    /*!
+     * \brief The exit action for this state.
+     * \note This function is called when the state is exited in response to a
+     * transition. The state is already popped from the machine stack \e before
+     * this function is called. The function may be \c NULL, in which case no
+     * action is taken.
+     *
+     * \param state The outgoing sub-state.
+     * \param machine The infinite state machine.
+     */
     void (*exit)(struct infinite_state *state, struct infinite_state_machine *machine);
 };
 
